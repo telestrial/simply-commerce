511bd404a712ee7789484abd2d019916b3e2e2d1
John Williams <telestrial@gmail.com>
2024-03-10 23:52:01 -0400

    add speed insights because why not?

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import Header from './_components/Header';
 import Footer from './_components/Footer';
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className='flex flex-col h-screen justify-between'>
           <Analytics />
+          <SpeedInsights />
           <Header />
           <main className='mb-auto overflow-y-auto'>{children}</main>
           <Footer />
