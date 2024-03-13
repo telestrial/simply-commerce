5590831ba348043cad8e92b040bab829434edb0d
John Williams <telestrial@gmail.com>
2024-03-13 14:49:55 -0400

    begin product stumping work

--- app/api/seed-db/route.ts
+++ app/api/seed-db/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   try {
-    const result = await sql`DROP TABLE Pets;`;
+    await sql`DROP TABLE Products;`;
+    const result =
+      await sql`CREATE TABLE Products ( Name varchar(255), Brand varchar(255),  );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
