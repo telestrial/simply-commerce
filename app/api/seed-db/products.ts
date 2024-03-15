53db88d7a43e786d9aa77399ac3100e9e6283526
John Williams <telestrial@gmail.com>
2024-03-15 15:01:18 -0400

    stump possible product interface

--- app/api/seed-db/products.ts
+++ app/api/seed-db/products.ts
@@ -1,4 +1,18 @@
-const products = [{}];
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
+
+const products: Product[] = [
+  {
+    id: 1,
+    name: 'Thing',
+    description: "it's a thing",
+    price: 10.99,
+  },
+];
 
 export function postgresProductInsert() {
   return ``;
