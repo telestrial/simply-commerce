f6193fb937a9b40ad8a8554e23faa09bab99cdaf
John Williams <telestrial@gmail.com>
2024-03-16 19:31:33 -0400

    rework create statment. are returns interrupting here?

--- app/api/seed-db/route.ts
+++ app/api/seed-db/route.ts
@@ -5,17 +5,12 @@ import { postgresProductInsertStatement } from './products';
 
 export async function GET(request: Request) {
   try {
-    await sql`DROP TABLE products;`;
-    const createResult = await sql`CREATE TABLE products ( 
-        id integer, 
-        name text,
-        brand text,
-        description text,
-        price NUMERIC(6, 2)  
-        );`;
+    const dropResult = await sql`DROP TABLE products;`;
+    const createResult =
+      await sql`CREATE TABLE products ( id integer, name text, brand text, description text, price NUMERIC(6, 2));`;
     const insertResult = await sql`${postgresProductInsertStatement()}`;
     return NextResponse.json(
-      { create: createResult, insert: insertResult },
+      { drop: dropResult, create: createResult, insert: insertResult },
       { status: 200 }
     );
   } catch (error) {
