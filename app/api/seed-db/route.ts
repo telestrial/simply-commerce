5e16c90fc2b2c48e63e1b4bd3b6fdfa2f7c9b24c
John Williams <telestrial@gmail.com>
2024-03-16 19:34:40 -0400

    uppercase database name

--- app/api/seed-db/route.ts
+++ app/api/seed-db/route.ts
@@ -5,9 +5,9 @@ import { postgresProductInsertStatement } from './products';
 
 export async function GET(request: Request) {
   try {
-    const dropResult = await sql`DROP TABLE products;`;
+    const dropResult = await sql`DROP TABLE Products;`;
     const createResult =
-      await sql`CREATE TABLE products ( id integer, name text, brand text, description text, price NUMERIC(6, 2));`;
+      await sql`CREATE TABLE Products ( id integer, name text, brand text, description text, price NUMERIC(6, 2));`;
     const insertResult = await sql`${postgresProductInsertStatement()}`;
     return NextResponse.json(
       { drop: dropResult, create: createResult, insert: insertResult },
