e5698c3641171d68650b54d8546b42809e5a9633
John Williams <telestrial@gmail.com>
2024-04-02 14:13:24 -0400

    try data fetching/seeding a differnt way

--- app/_components/ProductFeed.tsx
+++ app/_components/ProductFeed.tsx
@@ -1,17 +1,27 @@
-'use client';
-
 import { useEffect, useState } from 'react';
 import { sql } from '@vercel/postgres';
 
 import seedDB from '../lib/seedDB';
 
-export default function ProductFeed() {
-  const [data, setData] = useState(null);
+export default async function ProductFeed() {
+  let data;
 
-  useEffect(() =>
-    // Select SQl call
-    // if not that, seed
-    {}, []);
+  try {
+    data = await sql`SELECT * FROM products`;
+  } catch (e: any) {
+    if (e.message.includes('relation "products" does not exist')) {
+      console.log(
+        'Table does not exist, creating and seeding it with dummy data now...'
+      );
+      // Table is not created yet
+      await seedDB();
+      data = await sql`SELECT * FROM products`;
+    } else {
+      throw e;
+    }
+  }
+
+  console.log(data);
 
   return <div>Hello</div>;
 }
