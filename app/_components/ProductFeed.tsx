9b8fd400f4a2fc4941c31546f01cafc21e62b2ac
John Williams <telestrial@gmail.com>
2024-04-07 17:32:18 -0400

    add card

--- app/_components/ProductFeed.tsx
+++ app/_components/ProductFeed.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { sql } from '@vercel/postgres';
 
 import seedDB from '../lib/seedDB';
@@ -26,7 +26,26 @@ export default async function ProductFeed() {
   return (
     <div className='grid grid-cols-3 gap-4'>
       {products.map((product) => {
-        return <div key={product.id}>{product.name}</div>;
+        return (
+          <div
+            key={product.id}
+            className='card card-compact w-96 bg-base-100 shadow-xl'
+          >
+            <figure>
+              <Image
+                src='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+                alt='Shoes'
+              />
+            </figure>
+            <div className='card-body'>
+              <h2 className='card-title'>{product.name}</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className='card-actions justify-end'>
+                <button className='btn btn-primary'>Buy Now</button>
+              </div>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
