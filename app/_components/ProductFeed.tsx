a0d1baf03ec6447586a1cb1e027efffb8f9f86a0
John Williams <telestrial@gmail.com>
2024-04-08 13:48:23 -0400

    add flex before grid

--- app/_components/ProductFeed.tsx
+++ app/_components/ProductFeed.tsx
@@ -24,34 +24,39 @@ export default async function ProductFeed() {
   const { rows: products } = data;
 
   return (
-    <div className='grid grid-cols-4 gap-4'>
-      {products
-        .sort((a, b) => a.id - b.id)
-        .map((product) => {
-          return (
-            <div
-              key={product.id}
-              className='card card-compact w-96 bg-base-100 shadow-xl'
-            >
-              <figure>
-                <Image
-                  src={`/product-images/${product.id}.jpg`}
-                  alt='Shoes'
-                  width='300'
-                  height='150'
-                  objectFit='cover'
-                />
-              </figure>
-              <div className='card-body'>
-                <h2 className='card-title'>{product.name}</h2>
-                <p>{product.description}</p>
-                <div className='card-actions justify-end'>
-                  <button className='btn btn-primary'>Buy Now</button>
+    <div className='flex w-100 justify-center'>
+      <div className='grid grid-cols-3 gap-10'>
+        {products
+          .sort((a, b) => a.id - b.id)
+          .map((product) => {
+            return (
+              <div
+                key={product.id}
+                className='card card-compact w-96 bg-base-100 shadow-xl'
+              >
+                <figure>
+                  <Image
+                    src={`/product-images/${product.id}.jpg`}
+                    alt={product.name}
+                    layout='responsive'
+                    width='250'
+                    height='125'
+                    objectFit='fill'
+                  />
+                </figure>
+                <div className='card-body'>
+                  <div className='flex'>
+                    <h2 className='card-title'>{product.name}</h2>
+                    <p className='align-center'>${product.price}</p>
+                  </div>
+                  <div className='card-actions justify-end'>
+                    <button className='btn btn-primary'>Buy Now</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </div>
   );
 }
