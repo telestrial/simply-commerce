deb54e1696c9cb1092d0d7887d4c6eb8543648b5
John Williams <telestrial@gmail.com>
2024-04-14 16:50:48 -0400

    add some state settings

--- app/_store/index.ts
+++ app/_store/index.ts
@@ -4,6 +4,23 @@ interface ShoppingCartState {
   shoppingCart: string[];
 }
 
-const useShoppingCart = create((set) => ({
+export const useShoppingCart = create((set) => ({
   shoppingCart: [],
+  resetShoppingCart: () =>
+    set(() => ({
+      shoppingCart: [],
+    })),
+  addItemToCart: (itemID: string) =>
+    set((state: ShoppingCartState) => ({
+      shoppingCart: state.shoppingCart.push(itemID),
+    })),
+  removeItemFromCart: (itemID: string) =>
+    set((state: ShoppingCartState) => {
+      const modifiedShoppingCart = state.shoppingCart;
+      const position = modifiedShoppingCart.indexOf(itemID);
+      modifiedShoppingCart.splice(position, 1);
+      return {
+        shoppingCart: modifiedShoppingCart,
+      };
+    }),
 }));
