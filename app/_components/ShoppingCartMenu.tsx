8588f2d1b63a6ed5e2a9cd4a31aaf0c451048e95
John Williams <telestrial@gmail.com>
2024-04-24 12:33:03 -0400

    increase checkout menu width

--- app/_components/ShoppingCartMenu.tsx
+++ app/_components/ShoppingCartMenu.tsx
@@ -15,7 +15,7 @@ export default function ShoppingCartMenu() {
   return (
     <div
       tabIndex={0}
-      className='mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow'
+      className='mt-3 z-[1] card card-compact dropdown-content w-80 bg-base-100 shadow'
     >
       <div className='card-body'>
         <span className='font-bold text-lg'>
