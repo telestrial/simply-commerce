de3303a2423abea838ba3004203e44affaf398a0
John Williams <telestrial@gmail.com>
2024-05-02 20:37:35 -0400

    fix shopping cart total to account for numberOfItems

--- app/_store/useShoppingCartTotal.ts
+++ app/_store/useShoppingCartTotal.ts
@@ -2,5 +2,8 @@ import { useShoppingCart } from './store';
 
 export function useShoppingCartTotal() {
   const shoppingCart = useShoppingCart((state) => state.shoppingCart);
-  return shoppingCart.reduce((prev, curr) => prev + parseInt(curr.price), 0);
+  return shoppingCart.reduce(
+    (prev, curr) => prev + parseFloat(curr.price) * curr.numberOfItems,
+    0
+  );
 }
