e24d36fc37ceb0e9f15a19c9e4edc506edecd2d0
John Williams <telestrial@gmail.com>
2024-05-07 14:17:19 -0400

    add home to checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 export default function Checkout() {
   return (
-    <div className='flex w-100 justify-center pt-10'>Hello, Checkout!</div>
+    <div className='flex w-100 justify-center pt-10'>
+      <Link href='/'>Home</Link>
+      Hello, Checkout!
+    </div>
   );
 }
