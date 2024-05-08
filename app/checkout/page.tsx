66db2f83a8591fca59401002b002e64cec029ff8
John Williams <telestrial@gmail.com>
2024-05-08 15:27:09 -0400

    add shopping cart hook

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 
+import { useShoppingCart } from '../_store';
+
 export default function Checkout() {
+  const shoppingCartItems = useShoppingCart((state) => state.shoppingCart);
+
   return (
     <div className='flex w-100 justify-center pt-10'>
       <Link href='/'>Home</Link>
