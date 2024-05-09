33874d14247f44eaa257ebb3907f24a6bdd1d12a
John Williams <telestrial@gmail.com>
2024-05-09 17:52:55 -0400

    continue work on checkout screen

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -8,9 +8,12 @@ export default function Checkout() {
   const shoppingCartItems = useShoppingCart((state) => state.shoppingCart);
 
   return (
-    <div className='flex w-100 justify-center pt-10'>
-      <Link href='/'>Home</Link>
-      Hello, Checkout!
+    <div className='flex flex-col w-100 justify-center align-middle pt-10'>
+      <div className='w-52'>
+        <Link href='/' className='underline text-cyan-700'>
+          Home
+        </Link>
+      </div>
     </div>
   );
 }
