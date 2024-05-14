8b8b2d5c1637b5728837174d97bb288d72727e54
John Williams <telestrial@gmail.com>
2024-05-14 18:19:09 -0400

    fix checkout page width

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -8,16 +8,18 @@ export default function Checkout() {
   const shoppingCartItems = useShoppingCart((state) => state.shoppingCart);
 
   return (
-    <div className='flex flex-col w-100 justify-center align-middle pt-10'>
-      <div className='w-10'>
+    <div className='flex justify-center pt-10'>
+      <div className='w-6/12'>
         <Link href='/' className='underline text-cyan-700'>
           Home
         </Link>
-        {shoppingCartItems.map((item) => (
-          <div key={item.id}>
-            {item.name}: {item.numberOfItems}
-          </div>
-        ))}
+        <div className='pt-10'>
+          {shoppingCartItems.map((item) => (
+            <div key={item.id}>
+              {item.name}: {item.numberOfItems}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
